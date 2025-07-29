0b400e87b232902d8365454d87a21b422619822b
Cipster93 <stroeflorinc@yahoo.com>
2025-07-29 09:09:44 +0300

    Updates

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,68 +1,25 @@
-import React, { useEffect } from 'react';
-import { motion } from 'framer-motion';
-import Section1 from '../components/homeComponents/Section1';
-import Section2 from '../components/homeComponents/Section2';
-import Section3 from '../components/homeComponents/Section3';
-import Section4 from '../components/homeComponents/Section4';
-import useIsMobile from '../utils/UseViewportIsMobile';
-
-const sectionVariants = {
-  hidden: { opacity: 0, y: 40 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.9,
-      ease: 'easeOut',
-      delay: 0
-    }
-  }
-};
+import React from "react";
+import Section1 from "../components/homeComponents/Section1";
+import Section2 from "../components/homeComponents/Section2";
+import Section3 from "../components/homeComponents/Section3";
+import Section4 from "../components/homeComponents/Section4";
 
 const HomeView = () => {
-  const isMobile = useIsMobile();
-  const viewportSettings = { once: true, amount: isMobile ? 0.1 : 0.3 };
-
-
   return (
     <div className="home-view">
-      <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={viewportSettings}
-      >
+      <div>
         <Section1 />
-      </motion.div>
-
-      <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={viewportSettings}
-      >
+      </div>
+      <div>
         <Section2 />
-      </motion.div>
-
-      <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={viewportSettings}
-      >
+      </div>
+      <div>
         <Section3 />
-      </motion.div>
-
-      <motion.div
-        variants={sectionVariants}
-        initial="hidden"
-        whileInView="visible"
-        viewport={viewportSettings}
-      >
+      </div>
+      <div>
         <Section4 />
-      </motion.div>
+      </div>
     </div>
   );
 };
-
 export default HomeView;
